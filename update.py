8b6fbbbbe1f865c59ac3ed6cf0166f056da9e710
suryadeeprampur <suryadeep.rampur@gmail.com>
2025-07-03 09:59:09 +0530

    Update update.py

--- update.py
+++ update.py
@@ -75,7 +75,7 @@ if UPGRADE_PACKAGES.lower() == 'true':
 
 UPSTREAM_REPO = environ.get('UPSTREAM_REPO', '')
 if len(UPSTREAM_REPO) == 0:
-    UPSTREAM_REPO = "https://github.com/Tamilupdates/KPSML-X"
+    UPSTREAM_REPO = "https://github.com/suryadeeprampur/RDX-LEECH-BOT.git"
 
 UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH', '')
 if len(UPSTREAM_BRANCH) == 0:
